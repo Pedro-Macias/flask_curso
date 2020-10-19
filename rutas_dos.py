dcd7d996dc7c5bbcbaca5fb0f531dd1f2462c890
Pedro Macias <pin.macias@gmail.com>
2020-10-19 18:46:33 +0200

    agregamos gitignore

--- rutas_dos.py
+++ rutas_dos.py
@@ -16,7 +16,12 @@ def index():
 @app.route('/params/<name>') 
 # todo lo que venga detras del / lo almacena como name
 @app.route('/params/<name>/<int:num>') 
-# todo lo que venga detras del / lo almacena como name y un numero entero
+# todo lo que venga detras del / lo almacena como name y el siguiente / pedimos un numero entero
+'''
+http://127.0.0.1:9000/params/juan/25
+si no es un numero entero , la pagina da error , si no va el segundo parametro , muestra el mensaje
+
+´´´´
 def params(name = 'valor por defecto', num ='ningun numero'):
      return 'el parametro es {} {}'.format(name,num)
 
