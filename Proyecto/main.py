b0afedcd439d0a64c4ac360ba639a26548f121ce
Pedro Macias <pin.macias@gmail.com>
2020-10-23 20:01:59 +0200

    empezamos proyecto

---
+++ Proyecto/main.py
@@ -0,0 +1,13 @@
+# ARCHIVO DE INICIO
+from flask import Flask 
+from flask import render_template
+app = Flask(__name__) 
+@app.route('/') 
+def index():
+    title = 'Mi Proyecto Flask'
+    return render_template('index.html',title=title)
+
+    
+
+if __name__ == '__main__':
+    app.run(debug= True,port= 8000)
