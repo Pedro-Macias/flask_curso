f92dccdf714fff98d66722a16710c139d70c07b6
Pedro Macias <pin.macias@gmail.com>
2020-10-24 18:24:29 +0200

    validar un formulario con POST

--- Proyecto/forms.py
+++ Proyecto/forms.py
@@ -2,11 +2,21 @@ from flask_wtf import FlaskForm
 from wtforms import Form 
 from wtforms import StringField , TextField
 from wtforms.fields.html5 import EmailField
-
+from wtforms import validators
 
 
 class ComentarioForm(Form):
-    usuario = StringField('Usuario')
-    email = EmailField('Correo electronico')
+    usuario = StringField('Usuario',
+               [ 
+                   validators.Required(),
+                   validators.length(min=3, max = 10, message='ingrese un usuario minimo 3 caracteres y maximo 10')
+               ]
+               )
+    email = EmailField('Correo electronico',
+                [
+                validators.Required()
+                
+                ]
+                )
     comentario = TextField('Comentario')
 
--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,13 +1,25 @@
 # ARCHIVO DE INICIO
 from flask import Flask 
 from flask import render_template
-from flask_wtf import FlaskForm
+
+from flask import request
+
 import forms
 
+
 app = Flask(__name__) 
-@app.route('/') 
+# las rutas son accedidas por metodo GET , si queremos otro metodo habra que indicarlo
+@app.route('/', methods = ['GET', 'POST'] ) 
 def index():
-    formulario_coment = forms.ComentarioForm()
+    formulario_coment = forms.ComentarioForm(request.form)
+    if request.method == 'POST' and formulario_coment.validate():
+        usu= formulario_coment.usuario.data
+        mail = formulario_coment.email.data
+        coment = formulario_coment.comentario.data
+        return 'usuario: {} , email {} , comentario {}'.format(usu , mail , coment)
+        
+        
+
     title = 'Mi Proyecto Flask'
     return render_template('index.html',title=title, form =formulario_coment )
 
