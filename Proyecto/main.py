e8146d48639a2edebf3828417797ea5a393999d2
Pedro Macias <pin.macias@gmail.com>
2020-11-10 20:10:04 +0100

    crear config.py

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -5,17 +5,22 @@ from flask import request
 from flask import make_response
 from flask import session
 from flask import flash
+
+from config import DevelopmetConfig
+
 from flask import url_for
 from flask import redirect
 from flask_wtf import CsrfProtect
 
 import forms
+import json
 
+app = Flask(__name__)
+# aqui importamos el entorno que nos interese del config 
+app.config.from_object(DevelopmetConfig)
 
-app = Flask(__name__) 
 # vamos a ponerle una clave secreta , para validar ataques externos
-app.secret_key = 'mi_clave_secreta'
-csrf = CsrfProtect(app)
+csrf = CsrfProtect()
 
 # plantilla de error - el 404 es cuando el servidor no encuentra el recurso
 @app.errorhandler(404)
@@ -49,9 +54,9 @@ def login():
         username = login_form.username.data
         sucess_message = 'Bienvenido {}'.format(username)
         flash(sucess_message)
+        
         session['username'] = login_form.username.data
-
-    return render_template('login.html',title=title ,form= login_form)   
+    return render_template('login.html', form= login_form, title=title)   
 
 # hacemos una cookie
 @app.route('/cookie')
@@ -76,9 +81,16 @@ def coment():
     title = 'comentarios'
     return render_template('coment.html',title=title, form =formulario_coment )
 
+@app.route('/ajax-login', methods = ['POST'] )
+def ajax_login():
+   
+    username = request.form['username']
+    password = request.form['password']
+    # mi validacion
+    response ={'status': 200, 'username': username,'password':password, 'id':1}
+    return json.dumps( response)
 
-
-    
 
 if __name__ == '__main__':
-    app.run(debug= True,port= 8000)
+    csrf.init_app(app)
+    app.run(port= 8000)
--- Proyecto/static/js/jquery.js
+++ Proyecto/static/js/jquery.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    console.log("Pedro os saluda")
-})
+
+
+$(document).ready(function() {
+
+    function ajax_login(){
+        $.ajax({
+            url:'/ajax-login',
+            data:$('form').serialize(),
+            type: 'POST',
+            success: function(response){
+                console.log(response);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    }
+    $( "#login-form" ).submit(function( event ) {
+    
+    event.preventDefault();
+        ajax_login();
+            
+    });
+});
